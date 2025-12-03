0dd1f493ffdffed9fa0a86d92340cb07556049ed
breezko <10911394+breezko@users.noreply.github.com>
2025-12-03 10:39:29 +0100

    feat(day3): Added solution

---
+++ days/day04.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from utils.aoc import aoc_part
+from utils.io import read_input
+
+
+INPUT_FILE = Path(__file__).parents[1] / "inputs" / "raw" / "day04.txt"
+
+
+"""
+"""
+@aoc_part(day=4, part=1)
+def solve_part1():
+    return "TODO"
+
+
+
+"""
+"""
+@aoc_part(day=4, part=2)
+def solve_part2():
+    return "TODO"
+
+
+if __name__ == "__main__":
+    solve_part1()
+    solve_part2()
