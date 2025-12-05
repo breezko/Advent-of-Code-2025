c3b1e3c38914d11d79df7b2dc6954779a0e206df
Breezko <10911394+breezko@users.noreply.github.com>
2025-12-05 13:36:07 +0100

    feat(day05): Solution

--- utils/io.py
+++ utils/io.py
@@ -5,3 +5,11 @@ def read_input(path: Path) -> str:
 
 def read_lines(path: Path) -> list[str]:
     return path.read_text().splitlines()
+
+def read_split_by_character(path: Path, split_char: str = "\n\n"):
+    text = path.read_text().strip()          
+    part_x, part_y = text.split(split_char, 1)
+
+    X_lines = part_x.splitlines()
+    Y_lines = part_y.splitlines()
+    return X_lines, Y_lines
